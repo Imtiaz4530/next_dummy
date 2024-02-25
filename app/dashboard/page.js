7ece4cb19097a2d389c3b178c443ee1aa87ccb2f
Imtiaz_1 <smimtiaz58@gmail.com>
2024-02-25 20:26:08 +0600

    new commit

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const Dashboard = () => {
   return (
     <div>
-        <h1>Dashboard</h1>
-        <h3>Ahh I did some mistake</h3>
+      <h1>Dashboard</h1>
+      <h3>Ahh I did some mistake</h3>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
