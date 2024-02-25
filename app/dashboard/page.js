6d371e4027a043f526922c51d33a32b1d96fc633
Imtiaz_1 <smimtiaz58@gmail.com>
2024-02-25 20:36:26 +0600

    branchA updated

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -6,6 +6,7 @@ const Dashboard = () => {
       <h1>Dashboard</h1>
       <h3>Ahh I did some mistake</h3>
       <h6>I think I have to create a new branch.</h6>
+      <p>This is BrachA</p>
     </div>
   );
 };
